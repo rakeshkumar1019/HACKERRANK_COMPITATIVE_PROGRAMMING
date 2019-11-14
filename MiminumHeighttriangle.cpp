471faad55fda7cd73ab95648c069d506e9343b8c
rakeshkumar1019 <52422856+rakeshkumar1019@users.noreply.github.com>
2019-11-15 02:09:03 +0530

    Create MiminumHeighttriangle.cpp
    Minimum Height triangle

---
+++ MiminumHeighttriangle.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float base,area,height;
+    cin>>base>>area;
+    height=2*area/base;
+    int res=ceil(height);
+    cout<<res;
+
+}
