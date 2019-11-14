86aa3ba291c9e5f3624f953bd9910fcd311c58e8
rakeshkumar1019 <52422856+rakeshkumar1019@users.noreply.github.com>
2019-11-15 02:06:11 +0530

    Create MaximumDraws.cpp
    maximum Draws

---
+++ MaximumDraws.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    int res[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        res[i]=a[i]+1;
+        cout<<res[i]<<endl;
+    
+    } 
+        
+    
+}
